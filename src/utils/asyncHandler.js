a65805ed9c41eff634fc978a679de26359d86c46
Raaj1500s <mr.k.manikanta99@gmail.com>
2024-09-15 22:12:01 +0530

    Upto user model, video model, JWT, BCRYPT

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler = async (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err));
+    }
+}
+
+export {asyncHandler}
+
+/*
+const asyncHandlers = (func) => async (req,res,next) => {
+    try {
+        await func(req,res,next);
+    } catch (error) {
+        console.log(error);
+        res.status(error.statusCode).json(
+            {
+                success : false,
+                message: error.message
+            }
+        );
+        
+    }
+}
+*/
