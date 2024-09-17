f8b15b0a1c7c464a362c4edff5d4746815237303
Raaj1500s <mr.k.manikanta99@gmail.com>
2024-09-17 22:32:39 +0530

    Basic Route and Controller and Send Post request using Post Ma

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import {app} from './app.js'
+
 
 dotenv.config({
     path: './env'
@@ -7,7 +9,7 @@ dotenv.config({
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen( 8000, () => {
         console.log("Listening on port " + process.env.PORT);
         
     })
