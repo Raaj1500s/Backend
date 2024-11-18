0484202bd67a35e5f94ca09930a2d71910da32b9
raaj1500s <mr.k.manikanta99@gmail.com>
2024-11-18 23:32:10 +0530

    Fix some bugs

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,6 +6,7 @@ import {
     refreshAccessToken, 
     changeCurrentPassword, 
     getCurrentUser,
+    updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
     getUserChannelProfile,
